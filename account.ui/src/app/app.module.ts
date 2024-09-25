9f98887edd5486bf9cecae8858869be68da23430
felixalacampagne <42712089+felixalacampagne@users.noreply.github.com>
2024-09-25 08:38:51 +0200

    possible fix for blocked scrolling

--- account.ui/src/app/app.module.ts
+++ account.ui/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { BrowserModule, HammerModule } from '@angular/platform-browser';
+import { BrowserModule, HammerModule, HAMMER_GESTURE_CONFIG, HammerGestureConfig } from '@angular/platform-browser';
 import { importProvidersFrom, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { DatePipe } from '@angular/common';
@@ -9,6 +9,26 @@ import {AccountService} from '../shared/service/account.service';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { QrscannerComponent } from './qrscanner/qrscanner.component';
 
+
+// Magic required to get normal scrolling to work when swipe left is used.
+// WTF! Who in their right mind stops scrolling just to get swipe left!!!!!
+// Why don't any of the docs/examples mention this is required
+export class HammerConfigForNormalScroll extends HammerGestureConfig {
+
+   // Also requires entry in @NgModule 'providers' below
+   override overrides = <any> {
+       'pinch': { enable: false },
+       'rotate': { enable: false }
+   }
+
+   // Suggested in some places but seems to block the swipe
+   // override buildHammer(element: HTMLElement) {
+   //    let mc = new Hammer(element, {
+   //      touchAction: "auto",
+   //    });
+   //    return mc;
+   //  }   
+}
 
 @NgModule({ declarations: [
         AppComponent,
@@ -25,6 +45,8 @@ import { QrscannerComponent } from './qrscanner/qrscanner.component';
       AccountService, 
       DatePipe, 
       provideHttpClient(withInterceptorsFromDi()),
+      { provide: HAMMER_GESTURE_CONFIG, useClass: HammerConfigForNormalScroll }
    ]
 })
 export class AppModule { }
+
