0caa5ff1b26e72d57694b2e0eb8c416ce6c884bc
felixalacampagne <42712089+felixalacampagne@users.noreply.github.com>
2024-09-26 17:57:55 +0200

    Update AccountApplicationConfig.java

--- account.api/src/main/java/com/felixalacampagne/account/application/AccountApplicationConfig.java
+++ account.api/src/main/java/com/felixalacampagne/account/application/AccountApplicationConfig.java
@@ -1,23 +1,23 @@
 package com.felixalacampagne.account.application;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import com.felixalacampagne.account.model.Version;
 import com.felixalacampagne.account.persistence.repository.RepositoryConfig;
 import com.felixalacampagne.account.service.ServiceConfig;
+import com.felixalacampagne.account.standingorder.StandingOrderConfig;
 
 @Configuration
-@Import({RepositoryConfig.class, ServiceConfig.class})
+@Import({RepositoryConfig.class, ServiceConfig.class, StandingOrderConfig.class})
 
 public class AccountApplicationConfig
 {
 
 @Bean
 public Version version()
-{  
+{
    return new Version();
 }
 }
