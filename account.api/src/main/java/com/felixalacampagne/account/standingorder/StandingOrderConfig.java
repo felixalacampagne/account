70f2f85e3ce1beef69b84057e1cc3f44ecbf5aa4
felixalacampagne <42712089+felixalacampagne@users.noreply.github.com>
2024-09-26 17:53:56 +0200

    Update StandingOrderConfig.java

--- account.api/src/main/java/com/felixalacampagne/account/standingorder/StandingOrderConfig.java
+++ account.api/src/main/java/com/felixalacampagne/account/standingorder/StandingOrderConfig.java
@@ -1,1 +1,12 @@
+package com.felixalacampagne.account.standingorder;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class StandingOrderConfig
+{
+
+}
 
