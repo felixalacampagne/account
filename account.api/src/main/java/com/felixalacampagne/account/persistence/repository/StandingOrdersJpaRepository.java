ebce6d1df7ed175ba63030c8fbc630e1a1f47e1a
felixalacampagne <42712089+felixalacampagne@users.noreply.github.com>
2024-09-27 17:15:09 +0200

    Update StandingOrdersJpaRepository.java

--- account.api/src/main/java/com/felixalacampagne/account/persistence/repository/StandingOrdersJpaRepository.java
+++ account.api/src/main/java/com/felixalacampagne/account/persistence/repository/StandingOrdersJpaRepository.java
@@ -1,7 +1,8 @@
 package com.felixalacampagne.account.persistence.repository;
 
-import java.sql.Timestamp;
+import java.sql.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -12,6 +13,6 @@ import com.felixalacampagne.account.persistence.entities.StandingOrders;
 public interface StandingOrdersJpaRepository extends JpaRepository<StandingOrders, Long>
 {
 
-
-   public List<StandingOrders> findBySOEntryDateLessThanOrderBySOEntryDateAsc(Timestamp date);
+   public Optional<StandingOrders> findFirstBySOEntryDateLessThanOrderBySONextPayDateAsc(Date soEntryDate);
+   public List<StandingOrders> findBySOEntryDateLessThanOrderBySOEntryDateAsc(Date soEntryDate);
 }
