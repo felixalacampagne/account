fdcfdee3f5e2b03a22d197297ce790e2768d9532
Chris <chris@felixalacampagne.be>
2024-09-30 19:25:30 +0200

    more forking around with styles

--- account.ui/src/environments/environment.prod.ts
+++ account.ui/src/environments/environment.prod.ts
@@ -5,6 +5,6 @@ export const environment = {
   accountapi_host: "", // http://minnie",
   accountapi_app: "/jaccountapi/",
   envName: ' (J-LIVE)',  
-  uiversion: ' ui4.3',
+  uiversion: ' ui4.4a',
   folder: ''
 };
