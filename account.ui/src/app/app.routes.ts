2e72006fc036d4286c730f960abddc143e45cfb0
felixalacampagne <42712089+felixalacampagne@users.noreply.github.com>
2024-10-01 22:37:37 +0200

    Try to get reload to work

--- account.ui/src/app/app.routes.ts
+++ account.ui/src/app/app.routes.ts
@@ -5,5 +5,14 @@ import { TransactionsComponent } from './transactions/transactions.component';
 
 export const routes = [
    {path: 'transactions/:accid', title: "Transactions", component: TransactionsComponent},
-   {path: 'standingorders', title: "Standing Orders", component: StandingordersComponent}
+   {path: 'standingorders', title: "Standing Orders", component: StandingordersComponent},
+   {path: '**', redirectTo: '/'}
 ];
+
+// Refresh page doesn't work which means I'll have to waste even more time getting
+// something which is blindingly obvious to work again... wtf is it with these forking
+// people who create this shirt.
+// A clue seems to be
+// onsameurlnavigation : relaod
+// but the default is to do mothing (WTF -  why the fork do they think the user requested a forking refresh???)
+// and there is fork all indication how or where to change the value from the default...
