f93cd8de027e47363e4cad814bc8f71e89040a21
felixalacampagne <42712089+felixalacampagne@users.noreply.github.com>
2024-10-01 18:49:32 +0200

    Update app.routes.ts

--- account.ui/src/app/app.routes.ts
+++ account.ui/src/app/app.routes.ts
@@ -1,1 +1,9 @@
 // app.routes.ts
+import {Routes} from '@angular/router';
+import { StandingordersComponent } from './standingorders/standingorders.component';
+import { TransactionsComponent } from './transactions/transactions.component';
+
+export const routes = [
+   {path: 'transactions/:accid', title: "Transactions", component: TransactionsComponent},
+   {path: 'standingorders', title: "Standing Orders", component: StandingordersComponent}
+];
