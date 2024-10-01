fb50b77467057c5bae5c399f1dff62822d99c7cf
felixalacampagne <42712089+felixalacampagne@users.noreply.github.com>
2024-10-01 10:32:16 +0200

    Update BalanceService.java

--- account.api/src/main/java/com/felixalacampagne/account/service/BalanceService.java
+++ account.api/src/main/java/com/felixalacampagne/account/service/BalanceService.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.felixalacampagne.account.common.Utils;
 import com.felixalacampagne.account.persistence.entities.Transaction;
 import com.felixalacampagne.account.persistence.repository.TransactionJpaRepository;
 
