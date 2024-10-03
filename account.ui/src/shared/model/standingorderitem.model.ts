ad133c93b850396047c289ab31ec8176c3442c5b
felixalacampagne <42712089+felixalacampagne@users.noreply.github.com>
2024-10-03 16:33:58 +0200

    Create standingorderitem.model.ts

---
+++ account.ui/src/shared/model/standingorderitem.model.ts
@@ -0,0 +1,21 @@
+// standingorderitem.model.ts
+export class StandingOrderItem
+{
+   public soid : number = -1;
+   public soamount : string = '';
+   public socount : number = -1;
+   public sodesc : string = '';
+   public soentrydate : string = '';
+   public sonextpaydate : string = '';
+   public soperiod : number = -1;
+   public sotfrtype : string = '';
+   public accountid : number = -1;
+   public accountname : string = '';  
+   public token : string = '';
+    
+   constructor()
+   {
+       
+   }
+       
+}
