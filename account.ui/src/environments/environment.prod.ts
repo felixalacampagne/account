7db727e41aba797f5ce63613003c5f33f8f82981
felixalacampagne <42712089+felixalacampagne@users.noreply.github.com>
2024-10-11 17:57:28 +0200

    ui v4.5

--- account.ui/src/environments/environment.prod.ts
+++ account.ui/src/environments/environment.prod.ts
@@ -5,6 +5,6 @@ export const environment = {
   accountapi_host: "", // http://minnie",
   accountapi_app: "/jaccountapi/",
   envName: ' (J-LIVE)',  
-  uiversion: ' ui4.5b',
+  uiversion: ' ui4.5',
   folder: ''
 };
