95382eda2f43746031ffb1e7abb961daa0fa597b
felixalacampagne <42712089+felixalacampagne@users.noreply.github.com>
2024-11-25 17:10:52 +0100

    use phoneaccount for transfer account

--- account.api/src/main/java/com/felixalacampagne/account/persistence/repository/PhoneAccountJpaRepository.java
+++ account.api/src/main/java/com/felixalacampagne/account/persistence/repository/PhoneAccountJpaRepository.java
@@ -1,6 +1,10 @@
 package com.felixalacampagne.account.persistence.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.felixalacampagne.account.persistence.entities.PhoneAccount;
@@ -9,4 +13,8 @@ import com.felixalacampagne.account.persistence.entities.PhoneAccount;
 public interface PhoneAccountJpaRepository extends JpaRepository<PhoneAccount, Long>
 {
 
+   // NB. is accountId is null then 'p.accountId != :exaccid' is false!
+   @Query("SELECT p FROM PhoneAccount p where p.accountId != :exaccid and p.order not in (255) "
+        + "order by p.order DESC, p.desc ASC" )
+   List<PhoneAccount> findTransferAccounts(@Param("exaccid") Long excludeAccountId);
 }
