dbc3904d5b4d486fcb6b92593643ed1b8685e62c
felixalacampagne <42712089+felixalacampagne@users.noreply.github.com>
2025-01-27 17:24:21 +0100

    stnd mat theme, related account name iso id

--- account.api/src/main/java/com/felixalacampagne/account/model/TransferAccountItem.java
+++ account.api/src/main/java/com/felixalacampagne/account/model/TransferAccountItem.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 /* Contains the fields required to create and edit a PhoneAccount which aren't shown
  * in the normal usage in the transactions screen.
- * 
+ *
  * NB. THe values should be the actual values from the phone account
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -13,14 +13,16 @@ public class TransferAccountItem extends TfrAccountItem
    private Integer order;
    private String type;
    private String token;
-   public TransferAccountItem(Long id, Long relatedAccountId, String cptyAccountName, 
+   private String relatedAccountName;
+   public TransferAccountItem(Long id, Long relatedAccountId, String cptyAccountName,
          String cptyAccountNumber, String lastCommunication,
-         Integer order, String type,
+         Integer order, String type, String relatedAccountName,
          String token)
    {
       super(id, relatedAccountId, cptyAccountName, cptyAccountNumber, lastCommunication);
       this.order = order;
       this.type = type;
+      this.relatedAccountName = relatedAccountName;
       this.token = token;
    }
 
@@ -33,20 +35,28 @@ public class TransferAccountItem extends TfrAccountItem
    {
       return type;
    }
-   
+
+   @Override
    public String toString()
    {
       return "TransferAccountItem [id=" + getId()
+            + ", cptyAccountNumber=" + getCptyAccountNumber()
+            + ", cptyAccountName=" + getCptyAccountName()
+            + ", lastCommunication=" + getLastCommunication()
             + ", relatedAccountId=" + getRelatedAccountId()
+            + ", relatedAccountName=" + relatedAccountName
             + ", order=" + order
             + ", type=" + type
-            + ", cptyAccountNumber=" + getCptyAccountNumber() 
-            + ", cptyAccountName=" + getCptyAccountName() 
-            + ", lastCommunication=" + getLastCommunication() + "]";      
+            + "]";
    }
 
    public String getToken()
    {
       return token;
+   }
+
+   public String getRelatedAccountName()
+   {
+      return relatedAccountName;
    }
 }
