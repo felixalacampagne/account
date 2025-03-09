eb0f7d38ffba428a9f47aa2e1a13067d4df749c0
Chris <chris@felixalacampagne.be>
2025-03-09 22:04:11 +0100

    resync causes closed connection fatal error

--- account.api/src/main/java/com/felixalacampagne/account/service/ReSyncService.java
+++ account.api/src/main/java/com/felixalacampagne/account/service/ReSyncService.java
@@ -34,17 +34,22 @@ public class ReSyncService
             (this.accountService==null) ? "PROBLEM" : "GO",
             (this.balanceService==null) ? "PROBLEM" : "GO");
       
-//      this.accountService.getAccountList()
-//         .forEach( acc -> {
-//            log.info("reSyncBalances: sync date sorted balance for account:{} {}", acc.getId(), acc.getName());
-//            this.balanceService.calculateDatesortedBalances(acc.getId(), Optional.empty());
-//            
-//            log.info("reSyncBalances: sync sequence sorted balance for account:{} {}", acc.getId(), acc.getName());
-//            this.balanceService.calculateBalances(acc.getId(), Optional.empty());
-//   
-//            log.info("reSyncBalances: sync checked balance for account:{} {}", acc.getId(), acc.getName());
-//            this.balanceService.calculateCheckedBalances(acc.getId(), Optional.empty());
-//         });
+      this.accountService.getAccountList()
+         .forEach( acc -> {
+            
+            // Update of balance for all records (>5000) results in either leak detected exceptions 
+            // or the dreaded 'ClosedChannelException: null' so it looks like I'll need to
+            // find a way to do commits after x number of records. Probably something which could be
+            // solved by NOT using Access but I'm not ready for that yet...
+            log.info("reSyncBalances: sync date sorted balance for account:{} {}", acc.getId(), acc.getName());
+            this.balanceService.calculateDatesortedBalances(acc.getId(), Optional.empty());
+            
+            log.info("reSyncBalances: sync sequence sorted balance for account:{} {}", acc.getId(), acc.getName());
+            this.balanceService.calculateBalances(acc.getId(), Optional.empty());
+   
+            log.info("reSyncBalances: sync checked balance for account:{} {}", acc.getId(), acc.getName());
+            this.balanceService.calculateCheckedBalances(acc.getId(), Optional.empty());
+         });
       log.info("reSyncBalances: done");
    }
 }
